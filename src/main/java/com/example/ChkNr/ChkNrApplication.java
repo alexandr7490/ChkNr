a772a5851342607af0dcc578e6e706eadb2ff238
alex <alexandr7490@ya.ru>
2025-06-25 13:39:23 +0200

    Initial file was created.

---
+++ src/main/java/com/example/ChkNr/ChkNrApplication.java
@@ -0,0 +1,13 @@
+package com.example.ChkNr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChkNrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChkNrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ChkNr/ChkNrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ChkNr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChkNrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
